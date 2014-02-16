a33fc4302e3e686596c204b35c8f1bcc1467bcb9
Stephen Wood <stephen@intentsimplicity.com>
2014-02-16 11:45:56 -0800

    Little changes; added processor file.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-dailies
+Dailies
 =======
+Dailies is a system built to automate the process of scanning negative contact sheets into something more useful.
---
+++ processor.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+# Set paths
+$import_path = "/import";
+$export_path = "/export";
+$config_path = "/config";
+
+# Check the initial setup before doing anything else.
