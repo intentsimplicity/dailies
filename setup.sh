652ad3c337dfd26c18538442af2c92ae770aa84e
Stephen Wood <stephen@intentsimplicity.com>
2014-02-16 17:42:21 -0800

    Better communication.
    Added better communication to the setup.sh file.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/sh
 
 # Install dependencies
+echo "Installing dependencies. You may be asked to enter your password."
 sudo apt-get install git dcraw gphoto2 imagemagick
 
 # Create directories
+echo "Making directories..."
 mkdir 'import'
 mkdir 'export'
 mkdir 'config'
